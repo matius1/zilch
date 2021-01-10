73ef817f72e3d3c48fb38045e81b0ea1ad999c88
Mateusz Skocz <matius1@o2.pl>
2021-01-10 15:18:49 +0100

    Init project 2

---
+++ src/test/java/com/skocz/mateusz/zilch/ZilchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skocz.mateusz.zilch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZilchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
